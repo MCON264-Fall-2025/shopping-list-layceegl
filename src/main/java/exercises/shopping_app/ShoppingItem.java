d47fff72f691ac1ff09201acf4d48ba6024054e1
layceegl <lgluck2@student.touro.edu>
2025-12-28 21:48:13 -0500

    completed the assignment

--- src/main/java/exercises/shopping_app/ShoppingItem.java
+++ src/main/java/exercises/shopping_app/ShoppingItem.java
@@ -1,16 +1,35 @@
 package exercises.shopping_app;
 
-/**
- * Requirements for ShoppingItem
- * Fields:
- * String aisle; (you may also use int if you prefer numeric aisles)
- * String name;
- * A constructor that sets both fields.
- * getAisle() and getName() accessor methods (and toString() for nice printing).
- * Implement Comparable<ShoppingItem> so that items are ordered by:
- * Aisle (ascending), then
- * Name (alphabetically) when aisles are equal
- */
 public class ShoppingItem implements Comparable<ShoppingItem> {
-    //TODO
+    private int aisle;
+    private String name;
+
+    public ShoppingItem(int aisle, String name) {
+        this.aisle = aisle;
+        this.name = name;
+    }
+
+    public int getAisle() {
+        return aisle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(ShoppingItem other) {
+        // First compare aisles (ascending numbers)
+        int aisleCompare = Integer.compare(this.aisle, other.aisle);
+        if (aisleCompare != 0) {
+            return aisleCompare;
+        }
+        // If aisles equal, compare names alphabetically (ignore case)
+        return this.name.compareToIgnoreCase(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Aisle " + aisle + ": " + name;
+    }
 }
